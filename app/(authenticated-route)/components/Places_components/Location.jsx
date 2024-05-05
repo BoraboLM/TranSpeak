8722746273913b638e93857ba40e6209bf4de5cb
Lawrence-kun <borabolm.656.stud@cdd.edu.ph>
2024-05-05 19:30:30 +0800

    added conditional rendering for places

--- app/(authenticated-route)/components/Places_components/Location.jsx
+++ app/(authenticated-route)/components/Places_components/Location.jsx
@@ -7,9 +7,15 @@ export default function LocationCity() {
 
     return (
         <div className="flex items-center flex-col">
-            <h1 className=" text-center text-lg sm:text-xl md:text-2xl lg:2xl xl:text-4xl 2xl:text-4xl font-[600] tracking-wide text-slate-950">You are now in <span className="text-bold text-red-600">Dagupan</span></h1>
-            <p>Latitude: {coordinates?.latitude}</p>
-            <p>Longitude: {coordinates?.longitude}</p>
+            {coordinates ? (
+                <>
+                    <h1 className=" text-center text-lg sm:text-xl md:text-2xl lg:2xl xl:text-4xl 2xl:text-4xl font-[600] tracking-wide text-slate-950">You are now in <span className="text-bold text-red-600">Dagupan</span></h1>
+                    <p>Latitude: {coordinates.latitude}</p>
+                    <p>Longitude: {coordinates.longitude}</p>
+                </>
+            ) : (
+                <p className="text-center text-lg sm:text-xl md:text-2xl lg:2xl xl:text-4xl 2xl:text-4xl font-[600] tracking-wide text-slate-950">Your location is not available.</p>
+            )}
         </div>
     )
 }
