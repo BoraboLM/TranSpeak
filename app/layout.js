77beedce406985197cf825ff6f7924868e85f478
law <borabolawrence@gmail.com>
2024-10-09 19:42:30 +0800

    added google site verification

--- app/layout.js
+++ app/layout.js
@@ -60,6 +60,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+            <meta name="google-site-verification" content="Ah7o-pOZUNHM_1AbkOJkT2hwLzigcQy6Xj4IdAclQA0" />
+            </head>
             <body className={`${inter.className}`}>
                 {children}
                 <SpeedInsights />
